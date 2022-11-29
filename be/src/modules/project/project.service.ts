22e227cecacabf660c955954247d6011f456b074
sonmc <sonmc90@gmail.com>
2022-11-29 23:32:00 +0700

    add repo

--- be/src/modules/project/project.service.ts
+++ be/src/modules/project/project.service.ts
@@ -1,26 +1,33 @@
-import {Injectable} from '@nestjs/common';
-import {CreateProjectDto} from './dto/create-project.dto';
-import {UpdateProjectDto} from './dto/update-project.dto';
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { CreateProjectDto } from "./dto/create-project.dto";
+import { UpdateProjectDto } from "./dto/update-project.dto";
+import { ProjectRepository } from "./project.repository";
 
 @Injectable()
 export class ProjectService {
-	create(createProjectDto: CreateProjectDto) {
-		return 'This action adds a new project';
-	}
+  constructor(
+    @InjectRepository(ProjectRepository)
+    private projectRepository: ProjectRepository
+  ) {}
 
-	findAll() {
-		return `This action returns all project`;
-	}
+  create(createProjectDto: CreateProjectDto) {
+    return "This action adds a new project";
+  }
 
-	findOne(id: number) {
-		return `This action returns a #${id} project`;
-	}
+  findAll() {
+    return `This action returns all project`;
+  }
 
-	update(id: number, updateProjectDto: UpdateProjectDto) {
-		return `This action updates a #${id} project`;
-	}
+  findOne(id: number) {
+    return `This action returns a #${id} project`;
+  }
 
-	remove(id: number) {
-		return `This action removes a #${id} project`;
-	}
+  update(id: number, updateProjectDto: UpdateProjectDto) {
+    return `This action updates a #${id} project`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} project`;
+  }
 }
