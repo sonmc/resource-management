0ec3ea6399354b432c7b6c0d63ef2d6f75d432c1
sonmc <sonmc90@gmail.com>
2022-12-01 23:20:15 +0700

    use user repo

--- be/src/modules/user/user.service.ts
+++ be/src/modules/user/user.service.ts
@@ -1,28 +1,82 @@
-import { Injectable } from '@nestjs/common';
-import { UserRegisterRequestDto } from './dto/user-register.req.dto';
-import { User } from './entities/user.entity';
+import { HttpException, HttpStatus, Injectable, NotFoundException } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UpdateUserDto } from "./dto/update-user.dto";
+import { UserRegisterRequestDto } from "./dto/user-register.req.dto";
+import { UserDto } from "./dto/user.dto";
+import { User } from "./entities/user.entity";
+import { UserRepository } from "./user.repository";
 
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository
+  ) {}
 
-	async doUserRegistration(userRegister: UserRegisterRequestDto): Promise<User> {
-		const user = new User();
-		user.name = userRegister.name;
-		user.email = userRegister.email;
-		user.password = userRegister.password;
+  async doUserRegistration(
+    userRegister: UserRegisterRequestDto
+  ): Promise<User> {
+    const user = new User();
+    user.name = userRegister.name;
+    user.email = userRegister.email;
+    user.password = userRegister.password;
 
-		return await user.save();
-	}
+    return await user.save();
+  }
 
-	async getUserByEmail(email: string): Promise<User | undefined> {
-		return User.findOne({ where: { email } });
-	}
+  public async findAll(): Promise<User[]> {
+    return await this.userRepository.findAll();
+  }
 
-	async getUserById(id: number): Promise<User | undefined> {
-		return User.findOne({ where: { id } });
-	}
+  public async findOne(userId: number): Promise<User> {
+    const product = await this.userRepository.findById(userId);
+    if (!product) {
+      throw new NotFoundException(`User #${userId} not found`);
+    }
+    return product;
+  }
 
-	async findAll() {
-		return "This action return all users";
-	}
+  public async create(createUserDto: CreateUserDto): Promise<User> {
+    try {
+      return await this.userRepository.createUser(createUserDto);
+    } catch (err) {
+      throw new HttpException(err, HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  public async update(
+    userId: number,
+    updateUserDto: UpdateUserDto
+  ): Promise<User> {
+    const product = await this.userRepository.findOne(userId);
+    if (!product) {
+      throw new NotFoundException(`User #${userId} not found`);
+    }
+    return this.userRepository.editUser(userId, updateUserDto);
+  }
+
+  async getUserByEmail(email: string): Promise<UserDto[]> {
+    return await this.userRepository.getUsersByEmail(email);
+  }
+
+  public async remove(userId: number): Promise<void> {
+    await this.userRepository.delete(userId);
+  }
+
+  async geUsersByEmail(email: string): Promise<UserDto[]> {
+    return await this.userRepository.getUsersByEmail(email);
+  }
+
+  async findById(
+    id: number,
+    relations: string[] = [],
+    throwsException = false
+  ): Promise<UserDto> {
+    return await this.userRepository.getEntityById(
+      id,
+      relations,
+      throwsException
+    );
+  }
 }
