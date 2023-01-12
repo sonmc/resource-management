808830c76aa7f97e6f4b598b714ef23ced355557
SonMc <maicongson@zen8labs.com>
2023-01-12 16:39:45 +0700

    change file name

--- be/src/application/controllers/project/project.controller.ts
+++ be/src/application/controllers/project/project.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Delete, Get, Inject, ParseIntPipe, Post, Put, Query } from '@nestjs/common';
 import { ApiExtraModels, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { ProjectPresenter } from './dto/\u001Dproject.\u001Dpresenter';
+import { ProjectPresenter } from './dto/project.presenter';
 
 @Controller('projects')
 @ApiTags('projects')
