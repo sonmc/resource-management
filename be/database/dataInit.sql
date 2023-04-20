75fed7a889bce966bb4bee258f348d263c62f786
SonMc <maicongson@zen8labs.com>
2023-04-20 11:43:32 +0700

    add init data in sql

--- be/database/dataInit.sql
+++ be/database/dataInit.sql
@@ -1,22 +1,22 @@
-INSERT  INTO employee_management1.permissions VALUES (1,"2020/02/05","2020/02/05","login","Đăng nhập");
-INSERT  INTO employee_management1.permissions VALUES (2,"2020/02/05","2020/02/05","employee_get","Danh sách nhân viên");
-INSERT  INTO employee_management1.permissions VALUES (3,"2020/02/05","2020/02/05","employee_create","Thêm mới nhân viên");
-INSERT  INTO employee_management1.permissions VALUES (4,"2020/02/05","2020/02/05","project_get","Danh sách dự án");
-INSERT  INTO employee_management1.permissions VALUES (5,"2020/02/05","2020/02/05","role_get","Danh sách vai trò");
-INSERT  INTO employee_management1.permissions VALUES (6,"2020/02/05","2020/02/05","permission_get","Danh sách quyền");
-INSERT  INTO employee_management1.permissions VALUES (7,"2020/02/05","2020/02/05","vacation_get","Danh sách lịch nghỉ");
-INSERT  INTO employee_management1.permissions VALUES (8,"2020/02/05","2020/02/05","new_get","Danh sách tin tức");
-INSERT  INTO employee_management1.permissions VALUES (9,"2020/02/05","2020/02/05","new_create","Thêm mới bài viết");
-INSERT  INTO employee_management1.permissions VALUES (10,"2020/02/05","2020/02/05","candidate_get","Danh sách ứng viên");
+INSERT  INTO employee_management2.permissions VALUES (1,"2020/02/05","2020/02/05","login","Đăng nhập");
+INSERT  INTO employee_management2.permissions VALUES (2,"2020/02/05","2020/02/05","employee_get","Danh sách nhân viên");
+INSERT  INTO employee_management2.permissions VALUES (3,"2020/02/05","2020/02/05","employee_create","Thêm mới nhân viên");
+INSERT  INTO employee_management2.permissions VALUES (4,"2020/02/05","2020/02/05","project_get","Danh sách dự án");
+INSERT  INTO employee_management2.permissions VALUES (5,"2020/02/05","2020/02/05","role_get","Danh sách vai trò");
+INSERT  INTO employee_management2.permissions VALUES (6,"2020/02/05","2020/02/05","permission_get","Danh sách quyền");
+INSERT  INTO employee_management2.permissions VALUES (7,"2020/02/05","2020/02/05","vacation_get","Danh sách lịch nghỉ");
+INSERT  INTO employee_management2.permissions VALUES (8,"2020/02/05","2020/02/05","new_get","Danh sách tin tức");
+INSERT  INTO employee_management2.permissions VALUES (9,"2020/02/05","2020/02/05","new_create","Thêm mới bài viết");
+INSERT  INTO employee_management2.permissions VALUES (10,"2020/02/05","2020/02/05","candidate_get","Danh sách ứng viên");
 
 
-INSERT  INTO employee_management1.roles VALUES (1, "2020/02/05","2020/02/05","admin","")
+INSERT  INTO employee_management2.roles VALUES (1, "2020/02/05","2020/02/05","admin","")
 
-INSERT INTO employee_management1.users VALUES (1,"2020/02/02","2020/02/02","admin","admin@gmail.com","0900123123","admin","","$2b$12$3.7JhJKdBP8Lnoore/aMuOjBt8oiIPwyc1X5JLL6AB9oP2Mh1ato2",1,1,"","2020/02/09","2020/02/09",null,"2020/02/02",4,2)
+INSERT INTO employee_management2.users VALUES (1,"2020/02/02","2020/02/02","admin","admin@gmail.com","0900123123","admin","","$2b$12$3.7JhJKdBP8Lnoore/aMuOjBt8oiIPwyc1X5JLL6AB9oP2Mh1ato2",1,1,"","2020/02/09","Hn","2020/02/09",null,"2020/02/02",4,2)
 
-INSERT INTO employee_management1.users_roles VALUES (1,1)
+INSERT INTO employee_management2.users_roles VALUES (1,1)
+ 
  
   
  
- 
- 
+  
