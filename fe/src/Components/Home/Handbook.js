ba3093be068dd510ebcf735bf455b1f98bcc14fa
SonMc <maicongson@zen8labs.com>
2023-04-21 20:17:07 +0700

    add description handbook

--- fe/src/Components/Home/Handbook.js
+++ fe/src/Components/Home/Handbook.js
@@ -14,12 +14,21 @@ const Handbook = () => {
     };
     return (
         <React.Fragment>
-            <Col xl={12} md={12} onClick={() => showHandbookDetail()}>
+            <Col xl={12} md={12}>
                 <Card className="card-animate">
                     <CardBody>
-                        <div className="d-flex align-items-center">
-                            <div className="flex-grow-1 overflow-hidden">
-                                <img className="w-25 h-100" style={{ cursor: 'pointer' }} src={handbookImg} alt="" />
+                        <div className="d-flex align-middle">
+                            <div onClick={() => showHandbookDetail()}>
+                                <img className="w-75 h-100" style={{ cursor: 'pointer' }} src={handbookImg} alt="" />
+                            </div>
+                            <div className="flex-grow-1">
+                                <h5 className="mb-1 lh-base">Welcome to be a member of Zen8Labs! The first version of the handbook was written in 2022 when Zen8Labs was 3 years old to summarize the core values of Zen8Labs which helps members:</h5>
+                                <ul>
+                                    <li>Understand fully & clearly about the core values of our company, navigate behaviors & working style to form the unique culture</li>
+                                    <li>Have a common view of our company development strategy</li>
+                                    <li>Find and recruit other news members that are appropriate for key values and criteria</li>
+                                    <li>Communicate and develop Zen8Labs's branding</li>
+                                </ul>
                             </div>
                         </div>
                     </CardBody>
