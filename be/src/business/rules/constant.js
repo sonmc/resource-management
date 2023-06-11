1c8352feb4a698dc1587891037980d0e7c572ef1
SonMc <maicongson@zen8labs.com>
2023-06-11 20:03:01 +0700

    add vảiable const

---
+++ be/src/business/rules/constant.js
@@ -0,0 +1,5 @@
+export const ADMIN_ID = 1;
+export const STATUS_INACTIVE = 2;
+export const STATUS_ACTIVE = 1;
+export const REMOTE = 1;
+export const OFF = 0;
--- be/src/database/seed.sql
+++
@@ -1,3 +0,0 @@
-
-INSERT INTO users (id, username, password)
-VALUES	(2, 'Admin', ?);
