35c11d817c9a6d94505b49f8667ed9a18e7bcbf9
SonMc <maicongson@zen8labs.com>
2023-06-11 19:53:25 +0700

    Revert "change model"
    This reverts commit 18f35a47f16704f28324458967f264510436a633.

--- be/src/service/schemas/user.schema.ts
+++ be/src/service/schemas/user.schema.ts
@@ -1,85 +1,93 @@
-import { Entity, Column, Index, ManyToMany, JoinTable, BeforeInsert, OneToMany } from 'typeorm';
+import {
+  Entity,
+  Column,
+  Index,
+  ManyToMany,
+  JoinTable,
+  BeforeInsert,
+  OneToMany,
+} from "typeorm";
 
-import { BaseSchema } from './base.schema';
-import { RoleSchema } from './role.schema';
-import { hash } from '../../util/bcrypt.util';
-import { NewSchema } from './new.schema';
-import { VacationSchema } from './vacation.schema';
+import { BaseSchema } from "./base.schema";
+import { RoleSchema } from "./role.schema";
+import { hash } from "../../util/bcrypt.util";
+import { NewSchema } from "./new.schema";
+import { VacationSchema } from "./vacation.schema";
 
-@Entity({ name: 'user' })
+@Entity({ name: "user" })
 export class UserSchema extends BaseSchema {
-    @Column()
-    @Index({ unique: true })
-    username: string = '';
+  @Column()
+  @Index({ unique: true })
+  username: string = "";
 
-    @Column({ nullable: true })
-    full_name: string = '';
+  @Column({ nullable: true })
+  full_name: string = "";
 
-    @Column({ nullable: true })
-    nick_name: string = '';
+  @Column({ nullable: true })
+  nick_name: string = "";
 
-    @Column({ nullable: true })
-    email: string = '';
+  @Column({ nullable: true })
+  email: string = "";
 
-    @Column({ nullable: true })
-    phone_number: string = '';
+  @Column({ nullable: true })
+  phone_number: string = "";
 
-    @Column()
-    password: string = '';
+  @Column()
+  password: string = "";
 
-    @Column()
-    status: number = 0;
+  @Column()
+  status: number = 0;
 
-    @Column()
-    gender: boolean = false;
+  @Column()
+  gender: boolean = false;
 
-    @Column({ nullable: true })
-    avatar: string = '';
+  @Column({ nullable: true })
+  avatar: string = "";
 
-    @Column({ nullable: true })
-    dob: Date = new Date();
+  @Column()
+  dob: Date = new Date();
 
-    @Column({ nullable: true })
-    address: string = '';
+  @Column()
+  address: string = "";
 
-    @Column({ nullable: true })
-    introduce: string = '';
+  @Column({ nullable: true })
+  introduce: string = "";
 
-    @Column()
-    onboarding: Date = new Date();
+  @Column()
+  onboarding: Date = new Date();
 
-    @Column()
-    status_level: string = '';
+  @Column()
+  status_level: string = "";
 
-    @Column({ nullable: true })
-    chapterHead: number = 0;
+  @Column({ nullable: true })
+  chapterHead: number = 0;
 
-    @BeforeInsert()
-    async setPassword(password: string) {
-        this.password = await hash(password || this.password);
-    }
+  @BeforeInsert()
+  async setPassword(password: string) {
+    this.password = await hash(password || this.password);
+  }
 
-    @OneToMany(() => NewSchema, (n) => n.user, {
-        eager: true,
-    })
-    news: NewSchema[] | undefined;
+  @OneToMany(() => NewSchema, (n) => n.user, {
+    eager: true,
+  })
+  news: NewSchema[] | undefined;
 
-    @OneToMany(() => VacationSchema, (vacation) => vacation.user, {
-        eager: true,
-    })
-    vacations: VacationSchema[] | undefined;
+  @OneToMany(() => VacationSchema, (vacation) => vacation.user, {
+    eager: true,
+  })
+  vacations: VacationSchema[] | undefined;
 
-    @ManyToMany(() => RoleSchema, (role) => role.users)
-    @JoinTable({
-        name: 'users_roles',
-        joinColumn: { name: 'user_id', referencedColumnName: 'id' },
-        inverseJoinColumn: { name: 'role_id' },
-    })
-    roles: RoleSchema[] | undefined;
+  @ManyToMany(() => RoleSchema, (role) => role.users)
+  @JoinTable({
+    name: "users_roles",
+    joinColumn: { name: "user_id", referencedColumnName: "id" },
+    inverseJoinColumn: { name: "role_id" },
+  })
+  roles: RoleSchema[] | undefined;
 
-    @Column({ nullable: true })
-    last_login?: Date;
+  @Column({ nullable: true })
+  last_login?: Date;
 
-    @Column({ nullable: true })
-    hash_refresh_token: string = '';
+  @Column({ nullable: true })
+  hash_refresh_token: string = "";
 }
